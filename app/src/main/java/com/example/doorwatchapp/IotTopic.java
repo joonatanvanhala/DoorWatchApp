c89f6d8fbb23b2c500c90d5c8fea3b81543abe81
Joonatan Vanhala <joonatan@kodelabs.io>
2019-11-15 23:39:29 +0200

    Added aws sdk iot

---
+++ app/src/main/java/com/example/doorwatchapp/IotTopic.java
@@ -0,0 +1,16 @@
+package com.example.doorwatchapp;
+
+import com.amazonaws.services.iot.client.AWSIotMessage;
+import com.amazonaws.services.iot.client.AWSIotQos;
+import com.amazonaws.services.iot.client.AWSIotTopic;
+
+public class IotTopic extends AWSIotTopic {
+    public IotTopic(String topic, AWSIotQos qos) {
+        super(topic, qos);
+    }
+
+    @Override
+    public void onMessage(AWSIotMessage message) {
+        // called when a message is received
+    }
+}
--- app/src/main/java/com/example/doorwatchapp/MainActivity.java
+++ app/src/main/java/com/example/doorwatchapp/MainActivity.java
@@ -8,15 +8,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.amazonaws.services.iot.client.AWSIotException;
+import com.amazonaws.services.iot.client.AWSIotMqttClient;
+import com.amazonaws.services.iot.client.AWSIotQos;
+
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
 import org.eclipse.paho.client.mqttv3.IMqttToken;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
+
 public class MainActivity extends AppCompatActivity {
     private Button mButton;
-
+    private AWSIotMqttClient client;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -35,28 +40,41 @@ public class MainActivity extends AppCompatActivity {
         });
     }
     public void connect(){
-        String clientId = MqttClient.generateClientId();
-        MqttAndroidClient client =
-                new MqttAndroidClient(this.getApplicationContext(), "tcp://broker.hivemq.com:1883",
-                        clientId);
 
+        String clientEndpoint = "<prefix>.iot.<region>.amazonaws.com";       // replace <prefix> and <region> with your own
+        String clientId = "<unique client id>";                              // replace with your own client ID. Use unique client IDs for concurrent connections.
+        String certificateFile = "<certificate file>";                       // X.509 based certificate file
+        String privateKeyFile = "<private key file>";                        // PKCS#1 or PKCS#8 PEM encoded private key file
+
+        //TODO: load certificate files and pass it to keystore and keypassword
+        //client = new AWSIotMqttClient(clientEndpoint, clientId, keyStore, keyPassword);
         try {
-            IMqttToken token = client.connect();
-            token.setActionCallback(new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-                    // We are connected
-                    Log.d("", "onSuccess");
-                }
+            client.connect();
+        } catch (AWSIotException e) {
+            e.printStackTrace();
+        }
+    }
+    public void publish(){
+        String topic = "my/own/topic";
+        AWSIotQos qos = AWSIotQos.QOS0;
+        String payload = "any payload";
+        long timeout = 3000;                    // milliseconds
 
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    // Something went wrong e.g. connection timeout or firewall problems
-                    Log.d("", "onFailure");
+        IotMessage message = new IotMessage(topic, qos, payload);
+        try {
+            client.publish(message, timeout);
+        } catch (AWSIotException e) {
+            e.printStackTrace();
+        }
+    }
+    public void subscribe(){
+        String topicName = "my/own/topic";
+        AWSIotQos qos = AWSIotQos.QOS0;
 
-                }
-            });
-        } catch (MqttException e) {
+        IotTopic topic = new IotTopic(topicName, qos);
+        try {
+            client.subscribe(topic);
+        } catch (AWSIotException e) {
             e.printStackTrace();
         }
     }
